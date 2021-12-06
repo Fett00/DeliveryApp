4d5d1e818de6d4881255636adf4a9e273cc95ef6
Fett00 <musaew2014@gmail.com>
2021-12-06 15:05:10 +0300

    Add Images enum containing asset images

---
+++ DeliveryApp/Extension/Enums.swift
@@ -0,0 +1,13 @@
+//
+//  Enums.swift
+//  DeliveryApp
+//
+//  Created by Садык Мусаев on 06.12.2021.
+//
+
+import UIKit
+
+enum Images{
+    
+    static let emptyMeal = UIImage(named: "food-delivery")!
+}
