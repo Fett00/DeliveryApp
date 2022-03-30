fa366c31111fc729afb41c20f620ed7e8204531e
Fett00 <musaew2014@gmail.com>
2022-03-30 21:37:02 +0300

    Добавлен класс индикатора загрузки

---
+++ DeliveryApp/Views/CustomViews/LoadingBlurView.swift
@@ -0,0 +1,64 @@
+//
+//  LoadingBlurView.swift
+//  DeliveryApp
+//
+//  Created by Садык Мусаев on 30.03.2022.
+//
+
+import UIKit
+
+class LoadingBlurView: UIView{
+    
+    private let blurView: UIVisualEffectView
+    
+    private let activityView: UIActivityIndicatorView
+    
+    init(frame: CGRect, blurStyle: UIBlurEffect.Style, activityStyle: UIActivityIndicatorView.Style) {
+        
+        activityView = UIActivityIndicatorView(style: activityStyle)
+        activityView.frame = frame
+        
+        blurView = UIVisualEffectView(effect: UIBlurEffect(style: blurStyle))
+        
+        super.init(frame: frame)
+        
+        self.alpha = 0.0
+        self.addSubview(blurView, activityView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        activityView.frame = self.frame
+        blurView.frame = self.frame
+    }
+    
+    func enableActivityWithAnimation(completionBlock: @escaping () -> ()){
+        
+        self.activityView.startAnimating()
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: [ .curveEaseOut]) {
+            
+            self.alpha = 1.0
+        } completion: { _ in
+            
+            completionBlock()
+        }
+
+    }
+    
+    func disableActivityWithAnimation(completionBlock: @escaping () -> ()){
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [ .curveEaseOut]) {
+            
+            self.alpha = 0.0
+        } completion: { _ in
+            
+            self.activityView.stopAnimating()
+            completionBlock()
+        }
+
+    }
+}
